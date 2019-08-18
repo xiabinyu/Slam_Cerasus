8b89550712c6cb806da11ab67fe4609f47d2e9f5
bigcoffee_linux_mi <xiabinyu@sjtu.edu.cn>
2019-08-17 20:19:28 -0700

    add slam

---
+++ src/slam_cerasus_const.h
@@ -0,0 +1,9 @@
+//
+// Created by xiabinyu on 8/16/19.
+//
+
+#ifndef IPP_WS_SLAM_CERASUS_CONST_H
+#define IPP_WS_SLAM_CERASUS_CONST_H
+#define  CONST_LIDARMIN 0.1
+#define  CONST_LIDARMAX 1.0
+#endif //IPP_WS_SLAM_CERASUS_CONST_H
--- src/slam_cerasus_main.cpp
+++ src/slam_cerasus_main.cpp
@@ -8,36 +8,65 @@
 
 #include <std_msgs/Float64.h>
 #include <geometry_msgs/Vector3.h>
+#include "slam_cerasus_const.h"
+
 void Callback(std_msgs::Float64 ang);
 void realcall();
 void Callback2(std_msgs::Float64 _rpm);
 void Callback3(sensor_msgs::LaserScan lidar);
-cerasus_slam_class CSC;
-
+static cerasus_slam_class CSC;
+static ros::Publisher slam_pub;
 int main(int argc, char** argv){
 	ros::init(argc, argv, "slam_cerasus");
     //ROS_INFO_STREAM("1");
 	ros::NodeHandle nh;
     //ROS_INFO_STREAM("2");
-	ros::Subscriber sub = nh.subscribe("/imu", 1, Callback);
+	ros::Subscriber sub = nh.subscribe("/motor/steering_angle", 1, Callback);
 
     ros::Subscriber sub2 = nh.subscribe("/motor/rpm", 1, Callback2);
-	ros::spin();
+
+    ros::Subscriber sub3 = nh.subscribe("/scan", 1, Callback3);
+    slam_pub = nh.advertise<geometry_msgs::Vector3>("/slam", 1000);//Root Commandros::Publisher root_pub = nh.advertise<std_msgs::Int16>("Cerasus/root", 1000);//Root Command
+
+    ros::spin();
 	return 0;
 }
 
 void Callback(std_msgs::Float64 ang){
     //ROS_INFO_STREAM("3");
+    static int countCall=0;
+    std::cout<<"\n1:"<<countCall++;
     CSC.Update_Slam_imu(ang.data);
 }
 
 void Callback2(std_msgs::Float64 _rpm){
+
+    static int countCall=0;
+    std::cout<<"\n2:"<<countCall++;
     CSC.Update_Slam_rpm(_rpm.data);
 }
 
 void Callback3(sensor_msgs::LaserScan lidar){
-    
-}
+    //lidar.ranges[]
+
+    static int countCall=0;
+    std::cout<<"\n3**********:"<<countCall++;
+    double Data[360];
+    int count=0;
+    for(int i=0;i<360;i++){
+        if(lidar.ranges[i]>=CONST_LIDARMIN&&lidar.ranges[i]<=CHARCLASS_NAME_MAX){
+            Data[count]=lidar.ranges[i];
+        }
+    }
+    CSC.Update_Slam(Data,count);
+    double x,y,th;
+    geometry_msgs::Vector3 output;
+    CSC.Get_Slam(&x,&y,&th);
+    output.x=x;
+    output.y=y;
+    output.z=th;
+    slam_pub.publish(output);
+   }
     
   //  static tf::TransformBroadcaster br;
 //    br.sendTransform(tf::StampedTransform(transform,
